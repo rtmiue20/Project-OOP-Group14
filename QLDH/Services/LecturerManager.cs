3b0dc8f02a2a94dfb8ea88ae5c5e626a43fcba4b
Khang <khangtrieu.31241022785@st.ueh.edu.vn>
2026-05-27 22:03:31 +0700

    LecturerManager

--- QLDH/Services/LecturerManager.cs
+++ QLDH/Services/LecturerManager.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using QLDH.Entities;
+
+
+namespace QLDH.Service
+{
+    public class LecturerManager : BaseManager<Lecturer>
+    {
+        public LecturerManager() : base("lecturers.dat")
+        {
+        }
+
+
+        public override List<Lecturer> GetAll()
+        {
+            LoadFromFile();
+            return this.items;
+        }
+
+
+        protected override string GetId(Lecturer item)
+        {
+            return item.LecturerId;
+        }
+
+
+        public override List<Lecturer> Search(string keyword)
+        {
+            List<Lecturer> result = new List<Lecturer>();
+            foreach (Lecturer lec in GetAll())
+            {
+                if (lec.LecturerId.Contains(keyword) || lec.FullName.Contains(keyword) || lec.Department.Contains(keyword))
+                {
+                    result.Add(lec);
+                }
+            }
+            return result;
+        }
+    }
+}
--- Winforms/obj/Debug/net9.0-windows/Winforms.AssemblyInfo.cs
+++ Winforms/obj/Debug/net9.0-windows/Winforms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac9d83fd717a9a64c6b2eb8cc3b7eb28d934c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa6bb0d0a3fb6a4b56c968c75ca96267c10743cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
