fa6bb0d0a3fb6a4b56c968c75ca96267c10743cf
ngotrongphuc200306-vie <ngotrongphuc200306@gmail.com>
2026-05-27 21:56:44 +0700

    OfficialManager

--- QLDH/Services/OfficialManager.cs
+++ QLDH/Services/OfficialManager.cs
@@ -1,1 +1,41 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using QLDH.Entities;
+
+
+namespace QLDH.Service
+{
+    public class OfficialManager : BaseManager<Official>
+    {
+        public OfficialManager() : base("officials.dat")
+        {
+        }
+
+
+        public override List<Official> GetAll()
+        {
+            LoadFromFile();
+            return this.items;
+        }
+
+
+        protected override string GetId(Official item)
+        {
+            return item.StudentId;
+        }
+
+
+        public override List<Official> Search(string keyword)
+        {
+            List<Official> result = new List<Official>();
+            foreach (Official off in GetAll())
+            {
+                if (off.StudentId.Contains(keyword) || off.FullName.Contains(keyword) || off.ClassName.Contains(keyword))
+                {
+                    result.Add(off);
+                }
+            }
+            return result;
+        }
+    }
+}
--- Winforms/obj/Debug/net9.0-windows/Winforms.AssemblyInfo.cs
+++ Winforms/obj/Debug/net9.0-windows/Winforms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d656f8f1756d16c683413d7071691faea2c68351")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ac9d83fd717a9a64c6b2eb8cc3b7eb28d934c99")]
 [assembly: System.Reflection.AssemblyProductAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Winforms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
